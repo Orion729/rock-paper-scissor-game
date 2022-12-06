ac21703c2d164518b5e1a3156d2c4f4c033cd615
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2022-12-06 12:13:41 +0530

    update

---
+++ main.py
@@ -0,0 +1,59 @@
+import random
+
+choices = ['Rock','Paper','Scissor']
+computer = random.choice(choices)
+player = False
+cpu_score = 0
+player_score=0
+
+while True:
+    computer = random.choice(choices)
+    player = input("Choose Rock, Paper or Scissor: ").capitalize()
+    if player == computer:
+        print("\tYou and Computer chose the same. It's a Tie!")
+    elif player == 'Rock':
+        computer = random.choice(choices)
+        if computer == 'Paper':
+            print("\tPlayer:",player)
+            print("\tComputer:",computer)
+            print("\tYou lose!",computer,"covers",player)
+            cpu_score+=1
+        else:
+            print("\tPlayer:",player)
+            print("\tComputer:",computer)
+            print("\tYou win!",player,"smashes",computer)
+            player_score+=1
+    elif player == 'Paper':
+        computer = random.choice(choices)
+        if computer == 'Scissor':
+            print("\tPlayer:",player)
+            print("\tComputer:",computer)
+            print ("\tYou lose!",computer,"cuts",player)
+            cpu_score+=1
+        else:
+            print("\tPlayer:",player)
+            print("\tComputer:",computer)
+            print("\tYou win!",player,"covers",computer)
+            player_score+=1
+    elif player == 'Scissor':
+        computer = random.choice(choices)
+        if computer == 'Rock':
+            print("\tPlayer:",player)
+            print("\tComputer:",computer)
+            print ("\tYou lose!",computer,"smashes",player)
+            cpu_score+=1
+        else:
+            print("\tPlayer:",player)
+            print("\tComputer:",computer)
+            print("\tYou win!",player,"cuts",computer)
+            player_score+=1
+
+    elif player=='End':
+        print("--------Final Scores--------")
+        print(f"\tComputer:{cpu_score}")
+        print(f"\tPlayer:{player_score}")
+        break
+
+
+
+
